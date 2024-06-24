0f2dbebc05590a5255123cf68286c5be0a5f7ff0
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2024-06-24 18:13:07 +0900

    chore: Update dependencies and add symphonia crate for audio processing

--- src/data/test_data.rs
+++ src/data/test_data.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 use crossbeam_channel::Receiver;
 
 use super::RawDataLayer;
@@ -7,22 +9,32 @@ pub enum TestDataType {
     TestData1,
 }
 
-impl TestDataType {
-    pub fn voice_stream_receiver(&self) -> Receiver<Vec<f32>> {
-        match self {
-            TestDataType::TestData1 => {
-                let (sender, receiver) = crossbeam_channel::unbounded();
-                receiver
-            }
+pub struct TestData {
+    pub test_data_type: TestDataType,
+    sample_rate: Option<u32>,
+    voice_stream_receiver: Option<Receiver<Vec<f32>>>,
+    handles: Option<Vec<thread::JoinHandle<()>>>,
+}
+
+impl TestData {
+    pub fn new(test_data_type: TestDataType) -> Self {
+        TestData {
+            test_data_type,
+            sample_rate: None,
+            voice_stream_receiver: None,
+            handles: None,
         }
     }
 
+    pub fn voice_stream_receiver(&self) -> Receiver<Vec<f32>> {
+
+    }
+
     pub fn start(&mut self) {
-        match self {
-            TestDataType::TestData1 => {
-                // Start the test data
-            }
-        }
+        let file_path = match self.test_data_type {
+            TestDataType::TestData1 => "test_data/jfk_f32le.wav",
+        };
+        let (sender, receiver) = crossbeam_channel::unbounded();
     }
 
     pub fn sample_rate(&self) -> u32 {
@@ -42,5 +54,5 @@ impl RawDataLayer for TestDataType {
     fn sample_rate(&self) -> u32 {
         self.sample_rate()
     }
-    
+
 }
