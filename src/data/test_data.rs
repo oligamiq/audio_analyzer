c6bce9b791f5dcb1616040a720520fd6dadd2468
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2024-07-24 18:45:31 +0900

    update

--- src/data/test_data.rs
+++ src/data/test_data.rs
@@ -1,6 +1,8 @@
 use std::thread;
 
 use crossbeam_channel::Receiver;
+use symphonia::core::{audio::{AudioBuffer, SampleBuffer}, codecs::DecoderOptions, conv::IntoSample, formats::FormatOptions, io::MediaSourceStream, meta::MetadataOptions, probe};
+use tracing_subscriber::fmt::format::Format;
 
 use super::RawDataLayer;
 
@@ -35,6 +37,60 @@ impl TestData {
             TestDataType::TestData1 => "test_data/jfk_f32le.wav",
         };
         let (sender, receiver) = crossbeam_channel::unbounded();
+
+        self.voice_stream_receiver = Some(receiver);
+
+        let file = Box::new(std::fs::File::open(file_path).unwrap());
+
+        let mss = MediaSourceStream::new(file, Default::default());
+
+        let hint = probe::Hint::new();
+
+        let format_opts: FormatOptions = Default::default();
+        let metadata_opts: MetadataOptions = Default::default();
+        let decoder_opts: DecoderOptions = Default::default();
+
+        let probed =
+            symphonia::default::get_probe().format(&hint, mss, &format_opts, &metadata_opts).unwrap();
+
+        // Get the format reader yielded by the probe operation.
+        let mut format = probed.format;
+
+        // Get the default track.
+        let track = format.default_track().unwrap();
+
+        // Create a decoder for the track.
+        let mut decoder =
+            symphonia::default::get_codecs().make(&track.codec_params, &decoder_opts).unwrap();
+
+        // Store the track identifier, we'll use it to filter packets.
+        let track_id = track.id;
+
+        loop {
+            // Get the next packet from the format reader.
+            let packet = format.next_packet().unwrap();
+
+            // If the packet does not belong to the selected track, skip it.
+            if packet.track_id() != track_id {
+                continue;
+            }
+
+            // Decode the packet into audio samples, ignoring any decode errors.
+            match decoder.decode(&packet) {
+                Ok(audio_buf) => {
+                    // If the packet was decoded successfully, send the audio samples to the
+                    // receiver.
+                    let samples: AudioBuffer<f32> = audio_buf.make_equivalent();
+                    let mut buffer = Vec::with_capacity(samples.capacity());
+                    for sample in samples {
+                        buffer.push(sample[0]);
+                    }
+                    sender.send(buffer).unwrap();
+                }
+                Err(symphonia::core::errors::Error::DecodeError(_)) => (),
+                Err(_) => break,
+            }
+        }
     }
 
     pub fn sample_rate(&self) -> u32 {
