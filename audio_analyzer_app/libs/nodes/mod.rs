450883e97d41b51738c9256472836013ef9791ef
oligami <s20032@tokyo.kosen-ac.jp>
2024-10-17 13:48:43 +0900

    save

--- audio_analyzer_app/libs/nodes/mod.rs
+++ audio_analyzer_app/libs/nodes/mod.rs
@@ -1,5 +1,5 @@
-use config::{ConfigNodes, FftSizeNode};
-use egui::Ui;
+use config::{ConfigNodes, FftSizeNode, HopSizeNode};
+use egui::{Pos2, Sense, Ui, Vec2};
 use egui_editable_num::EditableOnText;
 use egui_snarl::{
     ui::{PinInfo, SnarlViewer},
@@ -8,9 +8,11 @@ use egui_snarl::{
 use layer::{LayerNodes, STFTLayerNode};
 use ndarray::{Array1, Array2};
 use num_complex::Complex;
+use pin_info::CustomPinInfo;
 
 pub mod config;
 pub mod layer;
+pub mod pin_info;
 pub mod utils;
 
 #[derive(serde::Serialize, serde::Deserialize)]
@@ -81,6 +83,7 @@ impl SnarlViewer<FlowNodes> for FlowNodesViewer {
     ) -> egui_snarl::ui::PinInfo {
         enum InputConfig {
             FftSize,
+            HopSize,
         }
 
         let mut input_config = None;
@@ -96,17 +99,29 @@ impl SnarlViewer<FlowNodes> for FlowNodesViewer {
                             })) = remote
                             {
                                 ui.label(format!("fft_size: {}", fft_size));
-                                return PinInfo::star();
+                                return CustomPinInfo::lock();
                             }
                         }
 
                         input_config = Some(InputConfig::FftSize);
 
-                        PinInfo::star().with_fill(egui::Color32::from_rgb(0, 0, 0))
+                        CustomPinInfo::setting(8)
                     }
                     1 => {
-                        ui.label("input STFTLayer");
-                        PinInfo::star().with_fill(egui::Color32::from_rgb(0, 0, 0))
+                        if let Some(pin) = pin.remotes.get(0) {
+                            let remote = &snarl[pin.node];
+                            if let FlowNodes::ConfigNodes(ConfigNodes::HopSizeNode(HopSizeNode {
+                                hop_size,
+                            })) = remote
+                            {
+                                ui.label(format!("hop_size: {}", hop_size));
+                                return CustomPinInfo::lock();
+                            }
+                        }
+
+                        input_config = Some(InputConfig::HopSize);
+
+                        CustomPinInfo::setting(8)
                     }
                     2 => {
                         ui.label("input STFTLayer");
@@ -117,7 +132,7 @@ impl SnarlViewer<FlowNodes> for FlowNodesViewer {
                 LayerNodes::MelLayer(_) => todo!(),
                 LayerNodes::SpectrogramDensityLayer(_) => todo!(),
             },
-            FlowNodes::ConfigNodes(config_nodes) => todo!(),
+            FlowNodes::ConfigNodes(_) => unreachable!(),
         };
 
         match input_config {
@@ -148,6 +163,33 @@ impl SnarlViewer<FlowNodes> for FlowNodesViewer {
                     }
                 }
             }
+            Some(InputConfig::HopSize) => {
+                let node = &mut snarl[pin.id.node];
+
+                let node = if let FlowNodes::LayerNodes(LayerNodes::STFTLayer(node)) = node {
+                    node
+                } else {
+                    unreachable!()
+                };
+
+                let STFTLayerNode { hop_size, .. } = node;
+                ui.label("hop_size");
+                let response = egui::TextEdit::singleline(hop_size)
+                    .clip_text(false)
+                    .desired_width(0.0)
+                    .margin(ui.spacing().item_spacing)
+                    .show(ui)
+                    .response;
+
+                if response.lost_focus() {
+                    node.hop_size.fmt();
+                    node.update();
+                } else if response.changed() {
+                    if node.hop_size.try_update() {
+                        node.update();
+                    }
+                }
+            }
             None => {}
         }
 
@@ -161,7 +203,7 @@ impl SnarlViewer<FlowNodes> for FlowNodesViewer {
         scale: f32,
         snarl: &mut egui_snarl::Snarl<FlowNodes>,
     ) -> egui_snarl::ui::PinInfo {
-        match &snarl[pin.id.node] {
+        match &mut snarl[pin.id.node] {
             FlowNodes::LayerNodes(layer_nodes) => match layer_nodes {
                 LayerNodes::STFTLayer(_) => {
                     ui.label("output STFTLayer");
@@ -170,7 +212,38 @@ impl SnarlViewer<FlowNodes> for FlowNodesViewer {
                 LayerNodes::MelLayer(_) => todo!(),
                 LayerNodes::SpectrogramDensityLayer(_) => todo!(),
             },
-            FlowNodes::ConfigNodes(config_nodes) => todo!(),
+            FlowNodes::ConfigNodes(config_nodes) => match config_nodes {
+                ConfigNodes::FftSizeNode(node) => {
+                    ui.end_row();
+                    if egui::TextEdit::singleline(&mut node.fft_size)
+                        .clip_text(false)
+                        .desired_width(0.0)
+                        .margin(ui.spacing().item_spacing)
+                        .show(ui)
+                        .response
+                        .lost_focus()
+                    {
+                        node.fft_size.fmt();
+                    }
+
+                    CustomPinInfo::setting(8)
+                }
+                ConfigNodes::HopSizeNode(node) => {
+                    ui.end_row();
+                    if egui::TextEdit::singleline(&mut node.hop_size)
+                        .clip_text(false)
+                        .desired_width(0.0)
+                        .margin(ui.spacing().item_spacing)
+                        .show(ui)
+                        .response
+                        .lost_focus()
+                    {
+                        node.hop_size.fmt();
+                    }
+
+                    CustomPinInfo::setting(8)
+                }
+            },
         }
     }
 
@@ -193,5 +266,23 @@ impl SnarlViewer<FlowNodes> for FlowNodesViewer {
             );
             ui.close_menu();
         }
+
+        ui.menu_button("config", |ui| {
+            if ui.button("fft_size").clicked() {
+                snarl.insert_node(
+                    pos,
+                    FlowNodes::ConfigNodes(ConfigNodes::FftSizeNode(FftSizeNode::default())),
+                );
+                ui.close_menu();
+            }
+
+            if ui.button("hop_size").clicked() {
+                snarl.insert_node(
+                    pos,
+                    FlowNodes::ConfigNodes(ConfigNodes::HopSizeNode(HopSizeNode::default())),
+                );
+                ui.close_menu();
+            }
+        });
     }
 }
