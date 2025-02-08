a1122a3eb2e47d6632405f1b548c8af18b3160c4
oligami <nziq53@gmail.com>
2025-02-09 03:20:45 +0900

    refactor: update FFT implementation and enhance data analysis for improved visualization

--- bench_data/src/bin/lpc.rs
+++ bench_data/src/bin/lpc.rs
@@ -1,10 +1,13 @@
 use audio_analyzer_core::prelude::*;
+use bench_data::lifter;
 use ndarray::*;
 use num_complex::Complex;
+use plotters::prelude::*;
 
 // lifterをしてから呼び出す
+// idct
 fn calculate_log_power_spectrum_envelope(cepstrum: Array1<f64>, n: usize) -> Array1<f64> {
-    let fft = rustfft::FftPlanner::new().plan_fft_forward(n);
+    let fft = rustfft::FftPlanner::new().plan_fft_inverse(n);
 
     let mut fft_input = cepstrum
         .iter()
@@ -48,7 +51,6 @@ fn main() {
         std::fs::create_dir("out").unwrap();
     }
 
-    let mut n = 0;
     let count = {
         let mut data = data.clone();
         data.start();
@@ -56,18 +58,125 @@ fn main() {
         while let Some(_) = data.try_recv() {
             count += 1;
         }
-
         count
     };
+
+    let mut n = 0;
     while let Some(data) = data.try_recv() {
         {
             n += 1;
-            if n != count / 2 {
+            if n != count / 2 && n % 50 != 0 {
                 continue;
             }
         }
 
-        println!("n: {:?}", n);
-        println!("data: {:?}", data);
+        analyze_data(
+            data.iter().map(|x| *x as f64).collect::<Array1<f64>>(),
+            sample_rate as f64,
+            n,
+        );
+
+        // println!("n: {:?}", n);
+        // println!("data: {:?}", data);
     }
+}
+
+fn fft(data: Array1<f64>) -> Array1<f64> {
+    let fft = rustfft::FftPlanner::new().plan_fft_forward(data.len());
+
+    let mut fft_input = data
+        .iter()
+        .map(|x| Complex::new(*x, 0.0))
+        .collect::<Vec<_>>();
+
+    fft.process(&mut fft_input);
+
+    let fft_output = fft_input.iter().map(|x| x.re).collect::<Array1<_>>();
+
+    fft_output
+}
+
+fn plot_view_data(view_data: Vec<(Vec<(f64, f64)>, String)>, x_max: f64, salt: usize) {
+    let file_name = format!("out/{salt}.svg");
+    let root_area = SVGBackend::new(&file_name, (1024, 768)).into_drawing_area();
+    root_area.fill(&WHITE).unwrap();
+
+    let root_area = root_area.titled("FFT", ("sans-serif", 60)).unwrap();
+
+    let mut chart = ChartBuilder::on(&root_area)
+        .margin(5)
+        .caption("FFT", ("sans-serif", 50).into_font())
+        .x_label_area_size(40)
+        .y_label_area_size(40)
+        .build_cartesian_2d(0.0..x_max, -40.0..40.0)
+        .unwrap();
+
+    chart.configure_mesh().draw().unwrap();
+
+    for (i, (data, title)) in view_data.iter().enumerate() {
+        chart
+            .draw_series(LineSeries::new(
+                data.iter().map(|(x, y)| (*x, *y)),
+                &Palette99::pick(i),
+            ))
+            .unwrap()
+            .label(title)
+            .legend(move |(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &Palette99::pick(i)));
+    }
+
+    chart
+        .configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()
+        .unwrap();
+
+    if let Err(e) = root_area.present() {
+        eprintln!("error: {}", e);
+    }
+}
+
+fn analyze_data(data: Array1<f64>, frame_rate: f64, salt: usize) {
+    let mut view_data: Vec<(Vec<(f64, f64)>, String)> = Vec::new();
+
+    let hanning = Array1::from_shape_fn(data.len(), |i| {
+        0.5 - 0.5 * (2.0 * std::f64::consts::PI * i as f64 / data.len() as f64).cos()
+    });
+    let hanning = hanning * data;
+    let fft_ = fft(hanning.clone());
+    let frequencies = (0..fft_.len())
+        .map(|x| x as f64 * frame_rate / fft_.len() as f64)
+        .collect::<Vec<_>>();
+    let log_power = fft_.mapv(|x| x.abs().log10() * 20.0);
+
+    view_data.push((
+        frequencies
+            .iter()
+            .zip(log_power.iter())
+            .map(|(x, y)| (*x, *y))
+            .collect(),
+        "log_power".to_string(),
+    ));
+
+    let mut cepstrum = fft(log_power.clone());
+    // let liftered = lifter(cepstrum.clone(), 30);
+    println!("cepstrum: {:?}", cepstrum.len());
+    for i in 0..cepstrum.len() {
+        if i > 30 {
+            cepstrum[i] = 0.0;
+        }
+    }
+    let spectral_envelope = calculate_log_power_spectrum_envelope(cepstrum, log_power.len());
+
+    view_data.push((
+        frequencies
+            .iter()
+            .zip(spectral_envelope.iter())
+            .map(|(x, y)| (*x, *y))
+            .collect(),
+        "spectral_envelope".to_string(),
+    ));
+
+    // plot_view_data(view_data, frame_rate / 2.0, salt);
+    plot_view_data(view_data, 4000.0, salt);
 }
